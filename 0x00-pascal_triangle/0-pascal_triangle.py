27b42fc16eb05699e02b1368def7e44cea78a9da
Torbary <torbarey020@gmail.com>
2023-07-30 17:19:14 +0100

    pascal triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if (n <= 0):
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row_int = [1]
+        if i > 0:
+            prev_row = triangle[-1]
+            for j in range(1, i):
+                row_int.append(prev_row[j - 1] + prev_row[j])
+
+        if i > 0:
+            row_int.append(1)
+        triangle.append(row_int)
+    return triangle
