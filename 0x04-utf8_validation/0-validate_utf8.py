7f6db49ca7894140b04f51aab56cf5f4ee9ad2ed
Torbary <torbarey020@gmail.com>
2023-08-23 08:18:00 +0100

    utf8 validation algorithm

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""utf8 validation Algorithm"""
+
+
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """utf8 validation"""
+    numberOfBytes = 0
+
+    for num in data:
+        bin_rep = format(num, "08b")[-8:]
+
+        if numberOfBytes == 0:
+            for bit in bin_rep:
+                if bit == "0":
+                    break
+                numberOfBytes += 1
+
+            if numberOfBytes == 0:
+                continue
+
+            if numberOfBytes == 1 or numberOfBytes > 4:
+                return False
+        else:
+            if not (bin_rep[0] == "1" and bin_rep[1] == "0"):
+                return False
+
+        numberOfBytes -= 1
+
+    return numberOfBytes == 0
