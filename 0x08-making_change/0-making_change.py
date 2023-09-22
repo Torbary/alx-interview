bf09190e90a6adf420f088eed7a905e1e60fe56e
Torbary <torbarey020@gmail.com>
2023-09-22 23:46:32 +0100

    removed typed-annotion

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,22 +2,11 @@
 """
 Given a pile of coins of different values, determine the fewest number of
 coins needed to meet a given amount total.
-
-    Prototype: def makeChange(coins, total)
-    Return: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    coins is a list of the values of the coins in your possession
-    The value of a coin will always be an integer greater than 0
-    You can assume you have an infinite number of each denomination of
-    coin in the list
-    Your solution’s runtime will be evaluated in this task
-
 """
 from typing import List
 
 
-def makeChange(coins: List[int], total: int) -> int:
+def makeChange(coins, total):
     """changeCoin"""
     # Create a list called dp to store minimum coin counts for various totals.
     # Initialize all values in dp to be greater than the given total.
