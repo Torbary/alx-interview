ef5c890070ed7bdb305969a22c5be52eb7b08be7
Torbary <torbarey020@gmail.com>
2023-09-23 12:13:14 +0100

    coins sorted

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,6 @@
 Given a pile of coins of different values, determine the fewest number of
 coins needed to meet a given amount total.
 """
-from typing import List
 
 
 def makeChange(coins, total):
@@ -16,23 +15,15 @@ def makeChange(coins, total):
     dp[0] = 0
 
     # Sort the available coins in ascending order.
-    coins.sort()
+    sorted_coins = sorted(coins, reverse=True)
 
     # Loop through each possible total from 1 to the given total.
     for t in range(1, total + 1):
         # Loop through each available coin value.
-        for c in coins:
+        for c in sorted_coins:
             # Check if the current coin can be used to make the current total.
             if t - c >= 0:
-                # Update dp[t] with the minimum between its current value
-                # and 1 + dp[t - c].
                 dp[t] = min(dp[t], 1 + dp[t - c])
             else:
-                # If the coin is too large to contribute to the current total,
-                # break the loop.
                 break
-
-    # If dp[total] is still greater than the initial total
-    # it means no valid combination of coins was found.
-    # In this case, return -1 to indicate it's not possible.
     return dp[total] if dp[total] != total + 1 else -1
